c945de0da6f40c62a61e58d5f51ea95efea9bba8
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-09 15:53:32 +0300

    Fixed Logout functionality

--- be/socialplatform/account/urls.py
+++ be/socialplatform/account/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # path("login/", user_login, name="login"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 
     path("", dashboard, name="dashboard"),
 ]
