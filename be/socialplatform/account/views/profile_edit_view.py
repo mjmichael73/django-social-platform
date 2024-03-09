900bef40d8b2c6596456a21ff1ed372cfc57acee
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-09 19:39:26 +0300

    Added django messages (session messages) for edit profile

--- be/socialplatform/account/views/profile_edit_view.py
+++ be/socialplatform/account/views/profile_edit_view.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from account.forms import UserEditForm, ProfileEditForm
 from django.shortcuts import render
+from django.contrib import messages
 
 
 @login_required
@@ -18,6 +19,9 @@ def profile_edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated successfully.")
+        else:
+            messages.error(request, "Error updating your profile.")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
