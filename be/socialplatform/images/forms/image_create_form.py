7b0cb8418863944a249e0755176f90449ac6bde1
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-10 12:49:04 +0300

    Added clean method for Image Create Form

--- be/socialplatform/images/forms/image_create_form.py
+++ be/socialplatform/images/forms/image_create_form.py
@@ -9,3 +9,11 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             "url": forms.HiddenInput
         }
+
+    def clean(self):
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg", "png"]
+        extension = url.split(".", 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError("The given URL does not match valid image extensions.")
+        return url
