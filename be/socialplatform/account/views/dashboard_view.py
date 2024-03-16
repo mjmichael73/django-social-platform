326e5a2fb918f87ed72aefe35eeccac6e897a9ca
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-16 19:36:24 +0300

    Changed Dashboard View

--- be/socialplatform/account/views/dashboard_view.py
+++ be/socialplatform/account/views/dashboard_view.py
@@ -1,13 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from actions.models import Action
 
 
 @login_required
 def dashboard(request):
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
     return render(
         request,
         "account/dashboard.html",
         {
-            "section": "dashboard"
+            "section": "dashboard",
+            "actions": actions
         }
     )
