8446efad9c3b91fe94d830bdf240010669ed9cce
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-18 13:15:14 +0300

    Added redis image views count

--- be/socialplatform/images/views/image_detail_view.py
+++ be/socialplatform/images/views/image_detail_view.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from images.models import Image
+import redis
+from django.conf import settings
+
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+)
 
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f'image:{image.id}:views')
     return render(
         request,
         "images/image/detail.html",
         {
             "section": "images",
-            "image": image
+            "image": image,
+            "total_views": total_views
         }
     )
